d79b425fd04dcd037f512e14b6cc241b794d4733
Mark Connelly <com0004@gmail.com>
2022-08-07 23:56:08 +1000

    Add functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getTypesenseClient, createBooksCollection } from '$lib/typesense';
+	import { getTypesenseClient, createBooksCollection, searchBooks } from '$lib/typesense';
 	import type { SearchResponse } from 'typesense/lib/Typesense/Documents';
 
 	// Create client
@@ -8,7 +8,14 @@
 	// Create books collection if not already exists
 	createBooksCollection().catch(console.log);
 
+	// Create to search input changes
+	let searchQuery = '';
 	let searchResult: SearchResponse<{}>;
+	async function search(q: string) {
+		searchResult = await searchBooks(q, 'title', 'ratings_count:desc');
+	}
+
+	$: search(searchQuery);
 </script>
 
 <h1>Typesense practice</h1>
@@ -16,10 +23,10 @@
 <p>Typesense is an open source text-search engine.</p>
 
 <label for="search">Search:</label>
-<input id="search" type="text" on:change />
+<input id="search" type="text" bind:value={searchQuery} />
 
 <div>
 	{#each searchResult?.hits || [] as hit}
-		<li>{hit.document}</li>
+		<div>{JSON.stringify(hit.document, null, 2)}</div>
 	{/each}
 </div>
