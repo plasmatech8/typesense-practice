a684adfe7f1e2a7a25edb716af73d7405a021783
Mark Connelly <com0004@gmail.com>
2022-08-07 22:46:57 +1000

    Init sveltekit app

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	let x = 1;
+</script>
+
+<h1>Typesense practice</h1>
+
+<p>Typesense is an open source text-search engine.</p>
+
+<label for="search">Search:</label>
+<input id="search" type="text" />
